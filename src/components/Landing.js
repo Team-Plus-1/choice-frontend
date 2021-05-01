7b575eb51101ecefabca37542a09a959cead49f9
lemonnuggets <63046776+lemonnuggets@users.noreply.github.com>
2021-05-01 17:45:26 +0530

    Fixed Landing Page css

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,20 +1,28 @@
 import React from "react";
 import "./Landing.css";
 import bg from "../images/Cover_5.svg";
+import squares from "../images/squares.svg";
 
 function Landing() {
-  return (
-    <div
-      className="Landing"
-      style={{
-        backgroundImage: { bg },
-      }}
-    >
-      <maintext className="maintext">Providing a Better</maintext>
-      Browsing<text className="text"> Choice</text>
-      <button className="landing__btn">Launch Extension</button>
-    </div>
-  );
+    return (
+        <div
+            className="Landing"
+            style={{
+                backgroundImage: { bg },
+            }}
+        >
+            <div className="image-wrapper">
+                <img className="background-image" src={squares} />
+            </div>
+            <div className="content-wrapper">
+                <h1 className="maintext">
+                    Providing a Better <br />
+                    Browsing <span className="text"> Choice</span>{" "}
+                </h1>
+                <button className="landing__btn">Launch Extension</button>
+            </div>
+        </div>
+    );
 }
 
 export default Landing;
