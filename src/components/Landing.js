0edd192f408eb6dcf763f05315c3772d2d67d113
Shriram-Sivanandam <ssshriram4@gmail.com>
2021-05-01 16:47:44 +0530

    minor design fixes

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,18 +1,17 @@
 import React from "react";
 import "./Landing.css";
 import bg from "../images/Cover_5.svg";
+import squares from "../images/squares.svg";
 
 function Landing() {
   return (
-    <div
-      className="Landing"
-      style={{
-        backgroundImage: { bg },
-      }}
-    >
+    <div className="Landing">
       <maintext className="maintext">Providing a Better</maintext>
-      Browsing<text className="text"> Choice</text>
+      Browsing<span className="text"> Choice</span>
       <button className="landing__btn">Launch Extension</button>
+      <div className="landing__squares">
+        <img src={squares} />
+      </div>
     </div>
   );
 }
