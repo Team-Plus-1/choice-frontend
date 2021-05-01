2ddbc6635da1d906f0665699332559ebf08464cd
lemonnuggets <63046776+lemonnuggets@users.noreply.github.com>
2021-05-02 03:52:00 +0530

    started with admin page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,45 +4,133 @@ import { Link, useHistory } from "react-router-dom";
 import "./Dashboard.css";
 
 export default function Dashboard() {
-  const [error, setError] = useState("");
-  const { currentUser, logout } = useAuth();
-  const history = useHistory();
+    const [error, setError] = useState("");
+    const { currentUser, logout } = useAuth();
+    const history = useHistory();
+    // const [reports, setReports] = useState([]);
+    // console.log("IN HERE");
 
-  async function handleLogout() {
-    setError("");
+    // const updateReports = async () => {
+    //     console.log(first);
+    //     if (first) {
+    //         try {
+    //             const response = await fetch(
+    //                 "http://localhost:5000/api/checkthreshold",
+    //                 requestOptions
+    //             );
 
-    try {
-      await logout();
-      history.push("/");
-    } catch {
-      setError("Failed to log out");
+    //             let result = await response.json();
+    //             first = false;
+    //             console.log(result);
+    //             let newReports;
+    //             if (result.data === null) {
+    //                 newReports = [];
+    //             } else {
+    //                 newReports = result.data;
+    //             }
+    //             setReports(newReports);
+    //         } catch (error) {
+    //             console.log("error", error);
+    //         }
+    //     }
+    // };
+    // updateReports();
+    async function handleLogout() {
+        setError("");
+
+        try {
+            await logout();
+            history.push("/");
+        } catch {
+            setError("Failed to log out");
+        }
     }
-  }
-
-  return (
-    <div className="dash__container">
-      <div className="dash__box">
-        <h2 className="dashboard__heading">Profile</h2>
-        {error && <alert variant="danger">{error}</alert>}
-        <h4 className="dash__email">Email: {currentUser.email}</h4>
-        <br></br>
-        <div className="btnContainer">
-          <button className="dashboard__updateBtn">
-            <Link to="/update-profile" className="dashboard__updateLink">
-              Update Profile
-            </Link>
-          </button>
-        </div>
-      </div>
-      <div>
-        <button
-          className="dashboard__logout"
-          variant="link"
-          onClick={handleLogout}
-        >
-          Log Out
-        </button>
-      </div>
-    </div>
-  );
+    if (currentUser.email !== "admin@admin.com") {
+        return (
+            <div className="dash__container">
+                <div className="dash__box">
+                    <h2 className="dashboard__heading">Profile</h2>
+                    {error && <alert variant="danger">{error}</alert>}
+                    <h4 className="dash__email">Email: {currentUser.email}</h4>
+                    <br></br>
+                    <div className="btnContainer">
+                        <button className="dashboard__updateBtn">
+                            <Link
+                                to="/update-profile"
+                                className="dashboard__updateLink"
+                            >
+                                Update Profile
+                            </Link>
+                        </button>
+                    </div>
+                </div>
+                <div>
+                    <button
+                        className="dashboard__logout"
+                        variant="link"
+                        onClick={handleLogout}
+                    >
+                        Log Out
+                    </button>
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className="two-col">
+                <div className="sidebar">
+                    <h2 className="dashboard__heading">Profile</h2>
+                    {error && <alert variant="danger">{error}</alert>}
+                    <h3 className="dash__email">Email: {currentUser.email}</h3>
+                    <div className="adminBtnContainer">
+                        <button className="dashboard__updateBtn">
+                            <Link
+                                to="/update-profile"
+                                className="dashboard__updateLink"
+                            >
+                                Update Profile
+                            </Link>
+                        </button>
+                    </div>
+                    <div>
+                        <button
+                            className="dashboard__logout"
+                            variant="link"
+                            onClick={handleLogout}
+                        >
+                            Log Out
+                        </button>
+                    </div>
+                </div>
+                <div className="mainbar"></div>
+            </div>
+            // <div className="dash__container admin">
+            //     <div className="dash__box admin">
+            //         <h2 className="dashboard__heading">Profile</h2>
+            //         {error && <alert variant="danger">{error}</alert>}
+            //         <h4 className="dash__email">Email: {currentUser.email}</h4>
+            //         <br></br>
+            // <div className="btnContainer">
+            //     <button className="dashboard__updateBtn">
+            //         <Link
+            //             to="/update-profile"
+            //             className="dashboard__updateLink"
+            //         >
+            //             Update Profile
+            //         </Link>
+            //     </button>
+            // </div>
+            //     </div>
+            //     <div>
+            //         <button
+            //             className="dashboard__logout"
+            //             variant="link"
+            //             onClick={handleLogout}
+            //         >
+            //             Log Out
+            //         </button>
+            //     </div>
+            // </div>
+        );
+    }
 }
--- src/components/SeeReport.js
+++ src/components/SeeReport.js
@@ -5,93 +5,109 @@ import "./SeeReport.css";
 let first = true;
 
 function SeeReport({ url }) {
-  const [reports, setReports] = useState([]);
-  console.log("IN HERE");
-  console.log(url);
-  var requestOptions = {
-    method: "GET",
-    redirect: "follow",
-  };
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
+    const [reports, setReports] = useState([]);
+    console.log("IN HERE");
+    console.log(url);
+    var requestOptions = {
+        method: "GET",
+        redirect: "follow",
+    };
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-  var raw = JSON.stringify({
-    url: url,
-  });
+    var raw = JSON.stringify({
+        url: url,
+    });
 
-  var requestOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: raw,
-    redirect: "follow",
-  };
+    var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+    };
 
-  const updateReports = async () => {
-    if (first) {
-      try {
-        const response = await fetch(
-          "http://localhost:5000/api/reports",
-          requestOptions
-        );
+    const updateReports = async () => {
+        console.log(first);
+        if (first) {
+            try {
+                const response = await fetch(
+                    "http://localhost:5000/api/reports",
+                    requestOptions
+                );
 
-        let { reply, data } = await response.json();
-        setReports(data);
-        first = false;
-      } catch (error) {
-        console.log("error", error);
-      }
-    }
-  };
-  updateReports();
-  // var reports = [
-  //     {
-  //         reportedURL: "dfsafa",
-  //         story:
-  //             "I was doing this and that person did this so it is here and I want to block this.",
-  //         tag: "fasdf",
-  //     },
-  //     {
-  //         reportedURL: "tyera",
-  //         story:
-  //             "So this is absolutely tragic for my friend. I feel bad for that person.",
-  //         tag: "ahgreyhu",
-  //     },
-  //     {
-  //         reportedURL: "example",
-  //         story: "This is very bad. I am devastated.",
-  //         tag: "test",
-  //     },
-  // ];
+                let result = await response.json();
+                first = false;
+                console.log(result);
+                let newReports;
+                if (result.data === null) {
+                    newReports = [];
+                } else {
+                    newReports = result.data;
+                }
+                setReports(newReports);
+            } catch (error) {
+                console.log("error", error);
+            }
+        }
+    };
+    updateReports();
+    // var reports = [
+    //     {
+    //         reportedURL: "dfsafa",
+    //         story:
+    //             "I was doing this and that person did this so it is here and I want to block this.",
+    //         tag: "fasdf",
+    //     },
+    //     {
+    //         reportedURL: "tyera",
+    //         story:
+    //             "So this is absolutely tragic for my friend. I feel bad for that person.",
+    //         tag: "ahgreyhu",
+    //     },
+    //     {
+    //         reportedURL: "example",
+    //         story: "This is very bad. I am devastated.",
+    //         tag: "test",
+    //     },
+    // ];
 
-  // let len = reports.length;
-  let count = 0;
-  const { currentUser } = useAuth();
+    // let len = reports.length;
+    let count = 0;
+    const { currentUser } = useAuth();
 
-  return (
-    <div className="see__container">
-      {reports.map(function (data, object) {
-        return (
-          <div className="card">
-            <div
-              className={`row ${count++ % 2 === 0 ? "right" : "left"}`}
-              key={data.reportId}
-            >
-              <div className="blue">
-                <h3>Report: {data.report_string}</h3>
-              </div>
-              <div className="orange">
-                <div className="tags">Tags: {data.categories.join(", ")}</div>
-                <div className={`vote ${currentUser === null ? "dno" : ""}`}>
-                  <i class="fas fa-angle-double-up voting"></i>
-                  <i class="fas fa-angle-double-down voting"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+    return (
+        <div className="see__container">
+            {reports.map(function (data, object) {
+                return (
+                    <div className="card">
+                        <div
+                            className={`row ${
+                                count++ % 2 === 0 ? "right" : "left"
+                            }`}
+                            key={data.reportId}
+                        >
+                            <div className="blue">
+                                <h3>Report: {data.report_string}</h3>
+                            </div>
+                            <div className="orange">
+                                <div className="tags">
+                                    Tags: {data.categories.join(", ")}
+                                </div>
+                                <div
+                                    className={`vote ${
+                                        currentUser === null ? "dno" : ""
+                                    }`}
+                                >
+                                    <i class="fas fa-angle-double-up voting"></i>
+                                    <i class="fas fa-angle-double-down voting"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
 export default SeeReport;
